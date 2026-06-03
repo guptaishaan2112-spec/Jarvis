c3d00fca84388e3585de51775cd7e41b3b9a09fd
Ishaan Gupta <guptaishaan2112@gmail.com>
2026-06-03 19:23:13 +0530

    jarvis can now repeat after me

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from email.mime import text
 
+import time
 import speech_recognition as sr
 import webbrowser
 import pyttsx3 #package to convert text to speech
@@ -7,37 +8,49 @@ import pyttsx3 #package to convert text to speech
 print('''   to start JARVIS say "HI JARVIS"
             say exit to "EXIT JARVIS"''')
 recognizer = sr.Recognizer()
-engine=pyttsx3.init()
 def speak(text):
+    engine=pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
+    engine.stop()
     
 if __name__ == "__main__":
-    with sr.Microphone() as source:
-        audio = recognizer.listen(source)
-    # recognize speech using Sphinx
-    while True: 
-        if recognizer.recognize_sphinx(audio) == "jarvis":
+    try:
+        with sr.Microphone() as source:
+            audio = recognizer.listen(source)
+        # recognize speech using Sphinx
+        if 'jarvis' in recognizer.recognize_google(audio).lower():
             a="JARVIS is listening"
             print(a)
             speak(a)
 
-        try:
-            a="JARVIS thinks you said " + recognizer.recognize_sphinx(audio)
-            print(a)
-            speak(a)
-        except sr.UnknownValueError:
-            a="JARVIS could not understand audio"
-            print(a)
-            speak(a)
-        except sr.RequestError as e:
-            a=f"JARVIS error; {0}"
-            print(a)
-            speak(a)
-        if recognizer.recognize_sphinx(audio) == "exit":
-            a="Goodbye"
-            print(a)
-            speak(a)
-            break
+            print('listening...')
+            
+            try:
+                while True:
+                    with sr.Microphone() as source:
+                        audio = recognizer.listen(source)
+                    a=recognizer.recognize_google(audio)
+                    print('jarvis thinks you said: ' + a)
+                    speak('jarvis thinks you said: ' + a)
+                    print('listening...')
+                    if recognizer.recognize_google(audio) == "exit":
+                        a="Goodbye"
+                        print(a)
+                        speak(a)
+                        break
+            except sr.UnknownValueError:
+                a="JARVIS could not understand audio"
+                print(a)
+                speak(a)
+            except sr.RequestError as e:
+                a=f"JARVIS error; {e}"
+                print(a)
+                speak(a)
+    except sr.UnknownValueError:
+        a="JARVIS could not understand audio"
+        print(a)
+        speak(a)
+                    
+            
         
-    
